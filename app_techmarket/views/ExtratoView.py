f8dceb7303dbc41ec5e9a8d130dd231ae3ad9504
Rodrigo Costa <rodrigo.costa94@yahoo.com.br>
2025-10-30 23:46:45 -0300

    Conclusão das paginas ( Minhas Compras e Extrato ); Ajuste em outras paginas

--- app_techmarket/views/ExtratoView.py
+++ app_techmarket/views/ExtratoView.py
@@ -4,7 +4,10 @@ from django.urls import reverse
 from django.contrib import messages
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import authenticate, login, logout 
+from app_techmarket.models.Movimentacao import Movimentacao
 
 def extrato_view(request):
+
+    movimentacoes_usuario = Movimentacao.objects.filter(usuario=request.user).order_by('-data_hora')
         
-    return render(request, 'extrato.html') 
+    return render(request, 'extrato.html', {'movimentacoes_usuario': movimentacoes_usuario}) 
--- app_techmarket/views/MinhasComprasView.py
+++ app_techmarket/views/MinhasComprasView.py
@@ -5,7 +5,10 @@ from django.urls import reverse
 from django.contrib import messages
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import authenticate, login, logout 
+from app_techmarket.models.Compra import Compra
 
 def minhas_compras_view(request):
+
+    compras_usuario = Compra.objects.filter(usuario=request.user).order_by('-data_hora')
         
-    return render(request, 'minhas_compras.html') 
+    return render(request, 'minhas_compras.html', {'compras_usuario': compras_usuario}) 
