b464f96860a8f99cc23ab39b45dfd1e4b4735e0f
Rodrigo Costa <rodrigo.costa94@yahoo.com.br>
2025-10-31 21:21:38 -0300

    Novos produtos adicionados

--- app_techmarket/models/Produto.py
+++ app_techmarket/models/Produto.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Produto(models.Model):
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
     titulo = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)    
     descricao = models.TextField()
     imagem = models.FileField(upload_to='media/', blank=True, null=True)
 
