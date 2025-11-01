2da5cbdaff25dd1aa16279bff437f489ff96d5dd
Rodrigo Costa <rodrigo.costa94@yahoo.com.br>
2025-11-01 15:47:43 -0300

    Validação de formulario de cadastro via JS

--- app_techmarket/models/PerfilUsuario.py
+++ app_techmarket/models/PerfilUsuario.py
@@ -2,9 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class PerfilUsuario(models.Model):
-    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, related_name="perfil_usuario")
     saldo = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     imagem = models.FileField(upload_to='media/imagens_perfil/', blank=True, null=True)
+    cpf = models.CharField(max_length=11, blank=True, null=True)
+    data_nascimento = models.CharField(max_length=10, blank=True, null=True)
+    numero_telefone = models.CharField(max_length=11, blank=True, null=True)
 
     def __str__(self):
         return f"{self.usuario.username} (Saldo: R$ {self.saldo})"
--- app_techmarket/views/CadastroUsuarioView.py
+++ app_techmarket/views/CadastroUsuarioView.py
@@ -6,10 +6,15 @@ from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import authenticate, login, logout 
 from django.contrib.auth.models import User
 from app_techmarket.models import PerfilUsuario
+from app_techmarket.models.Movimentacao import Movimentacao
 
 def cadastro_view(request):
 
     if request.method == "POST":
+        nome_completo = request.POST.get("nome_completo")
+        cpf = request.POST.get("cpf")
+        data_nascimento = request.POST.get("data_nascimento")
+        numero_telefone = request.POST.get("telefone")
         usuario = request.POST.get("usuario")
         email = request.POST.get("email")
         senha1 = request.POST.get("senha1")
@@ -24,9 +29,35 @@ def cadastro_view(request):
         if User.objects.filter(username=usuario).exists():
             messages.error(request, "Usuário já existe.")
             return redirect('cadastro_view')
+        
+        ano = data_nascimento.split("-")[0]
+        mes = data_nascimento.split("-")[1]
+        dia = data_nascimento.split("-")[2]
 
-        novo_usuario = User.objects.create_user(username=usuario, email=email, password=senha1)
-        PerfilUsuario.objects.create(usuario=novo_usuario, saldo=saldo, imagem=imagem)
+        data_nascimento = f"{dia}/{mes}/{ano}"
+
+        novo_usuario = User.objects.create_user(
+            username=usuario, 
+            first_name=nome_completo.split(" ")[0],
+            last_name=" ".join(nome_completo.split(" ")[1:]),
+            email=email, 
+            password=senha1,
+        )
+
+        PerfilUsuario.objects.create(
+            usuario=novo_usuario, 
+            saldo=saldo, 
+            imagem=imagem,
+            cpf=cpf,
+            data_nascimento=data_nascimento,
+            numero_telefone=numero_telefone,
+        )
+
+        Movimentacao.objects.create(
+            usuario=novo_usuario,
+            valor=saldo,
+            tipo_movimentacao="Deposito"
+        )
 
         messages.success(request, "Cadastro realizado com sucesso! Faça login para continuar.")
         return redirect('login')
